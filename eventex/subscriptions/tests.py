11bbc0ff259c78211fc4e9bd79a01c1e6d50bda3
Henrique Bastos <henrique@bastos.net>
2017-06-04 18:17:09 -0300

    SubscribeTest.test_template

--- eventex/subscriptions/tests.py
+++ eventex/subscriptions/tests.py
@@ -6,4 +6,8 @@ class SubscribeTest(TestCase):
 		"""Get /inscricao/ must return status code 200"""
 		response = self.client.get('/inscricao/')
 		self.assertEqual(200, response.status_code)
-		
+	
+	def test_template(self):
+		"""Must use subscriptions/subscription_form.html"""
+		response = self.client.get('/inscricao/')
+		self.assertTemplateUsed(response, 'subscriptions/subscription_form.html')
