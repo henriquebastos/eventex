43bf5a1b23cad20fc63d3a3a8667e23cbb24a4e4
Henrique Bastos <henrique@bastos.net>
2017-06-04 18:06:06 -0300

    HomeTest.setUp

--- eventex/core/tests.py
+++ eventex/core/tests.py
@@ -2,12 +2,13 @@ from django.test import TestCase
 
 
 class HomeTest(TestCase):
+	def setUp(self):
+		self.response = self.client.get('/')
+
 	def test_get(self):
 		"""GET / must return status code 200"""
-		response = self.client.get('/')
-		self.assertEqual(200, response.status_code)
+		self.assertEqual(200, self.response.status_code)
 
 	def test_template(self):
 		"""Must use index.html"""
-		response = self.client.get('/')
-		self.assertTemplateUsed(response, 'index.html')
+		self.assertTemplateUsed(self.response, 'index.html')
