8315e30c164e89620aef5544b466d30f3d67a286
Henrique Bastos <henrique@bastos.net>
2017-06-04 18:17:09 -0300

    SubscribeTest.test_template

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def subscribe(request):
-	return HttpResponse()
+	return render(request, 'subscriptions/subscription_form.html')
