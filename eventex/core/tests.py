27a6ef7551f5e83a374b0f7f62585db713145a62
Henrique Bastos <henrique@bastos.net>
2017-06-04 18:03:49 -0300

    HomeTest.test_get

--- eventex/core/tests.py
+++ eventex/core/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class HomeTest(TestCase):
+	def test_get(self):
+		response = self.client.get('/')
+		self.assertEqual(200, response.status_code)
