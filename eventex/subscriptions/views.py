98cb78278505d389c3ef7822e3362d90779f0ae7
Henrique Bastos <henrique@bastos.net>
2017-06-05 10:28:29 -0300

    SubscribeInvalidPost.test_form_has_errors

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -21,7 +21,7 @@ def subscribe(request):
 			return HttpResponseRedirect('/inscricao/')
 		else:
 			return render(request, 'subscriptions/subscription_form.html',
-				          {'form': SubscriptionForm()})
+				          {'form': form})
 	else:
 		context = {'form': SubscriptionForm()}
 		return render(request, 'subscriptions/subscription_form.html', context)
