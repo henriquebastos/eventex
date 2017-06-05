ba7ccf72ef564ff29e2b45b86fbf138b8bc8ed54
Henrique Bastos <henrique@bastos.net>
2017-06-05 13:50:23 -0300

    SubscriptionModelAdmin.search_fields

--- eventex/subscriptions/admin.py
+++ eventex/subscriptions/admin.py
@@ -5,6 +5,7 @@ from eventex.subscriptions.models import Subscription
 class SubscriptionModelAdmin(admin.ModelAdmin):
 	list_display = ('name', 'email', 'phone', 'cpf', 'created_at')
 	date_hierarchy = 'created_at'
+	search_fields = ('name', 'email', 'phone', 'cpf', 'created_at')
 
 
 admin.site.register(Subscription, SubscriptionModelAdmin)
