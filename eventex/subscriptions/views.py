0bdb82e1e61a4da29305757dfe1165a91c15b225
Henrique Bastos <henrique@bastos.net>
2017-06-05 10:20:44 -0300

    Refactor subscribe view to use form.cleaned_data

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -7,15 +7,16 @@ from eventex.subscriptions.forms import SubscriptionForm
 
 def subscribe(request):
 	if request.method == 'POST':
-		context = dict(name='Henrique Bastos', cpf='12345678901',
-			           email='henrique@bastos.net', phone='21-99618-6180')
+		form = SubscriptionForm(request.POST)
+		form.full_clean()
+
 		body = render_to_string('subscriptions/subscription_email.txt',
-			                     context)
+			                     form.cleaned_data)
 
 		mail.send_mail('Confirmação de inscrição',
 					   body,
 					   'contato@eventex.com.br',
-					   ['contato@eventex.com.br', 'henrique@bastos.net'])
+					   ['contato@eventex.com.br', form.cleaned_data['email']])
 
 		return HttpResponseRedirect('/inscricao/')
 	else:
