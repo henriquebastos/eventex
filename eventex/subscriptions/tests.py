a37280f0ad18dcb2b9b1564b7f0d880229c3a819
Henrique Bastos <henrique@bastos.net>
2017-06-05 09:44:48 -0300

    SubscribeTest.test_csrf

--- eventex/subscriptions/tests.py
+++ eventex/subscriptions/tests.py
@@ -16,7 +16,11 @@ class SubscribeTest(TestCase):
 	def test_html(self):
 		"""Html must contain input tags"""
 		self.assertContains(self.resp, '<form')
-		self.assertContains(self.resp, '<input', 5)
+		self.assertContains(self.resp, '<input', 6)
 		self.assertContains(self.resp, 'type="text"', 3)
 		self.assertContains(self.resp, 'type="email"')
 		self.assertContains(self.resp, 'type="submit"')
+
+	def test_csrf(self):
+		"""Html must contain csrf"""
+		self.assertContains(self.resp, 'csrfmiddlewaretoken')
