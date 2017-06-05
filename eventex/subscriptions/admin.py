9dc9a209bd86bad09f85162140422ee17408ca68
Henrique Bastos <henrique@bastos.net>
2017-06-05 13:54:37 -0300

    SubscriptionModelAdmin.subscribed_today

--- eventex/subscriptions/admin.py
+++ eventex/subscriptions/admin.py
@@ -1,11 +1,19 @@
 from django.contrib import admin
+from django.utils.timezone import now
 from eventex.subscriptions.models import Subscription
 
 
 class SubscriptionModelAdmin(admin.ModelAdmin):
-	list_display = ('name', 'email', 'phone', 'cpf', 'created_at')
+	list_display = ('name', 'email', 'phone', 'cpf', 'created_at',
+		            'subscribed_today')
 	date_hierarchy = 'created_at'
 	search_fields = ('name', 'email', 'phone', 'cpf', 'created_at')
+
+	def subscribed_today(self, obj):
+		return obj.created_at.date() == now().date()
+
+	subscribed_today.short_description = 'inscrito hoje?'
+	subscribed_today.boolean = True
 
 
 admin.site.register(Subscription, SubscriptionModelAdmin)
