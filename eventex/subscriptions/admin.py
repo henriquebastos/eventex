51c3e28f47a3fa1a4f4bf49ee055a2ced8ac455f
Henrique Bastos <henrique@bastos.net>
2017-06-05 19:11:04 -0300

    SubscriptionModelAdminTest.test_has_action

--- eventex/subscriptions/admin.py
+++ eventex/subscriptions/admin.py
@@ -8,7 +8,9 @@ class SubscriptionModelAdmin(admin.ModelAdmin):
 		            'subscribed_today', 'paid')
 	date_hierarchy = 'created_at'
 	search_fields = ('name', 'email', 'phone', 'cpf', 'created_at')
-	list_filter = ('paid', 'created_at')
+	list_filter = ('paid',  'created_at')
+
+	actions = ['mark_as_paid']
 
 	def subscribed_today(self, obj):
 		return obj.created_at.date() == now().date()
