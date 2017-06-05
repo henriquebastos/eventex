db73790c2d26561181c838d6c49504d1c536f41c
Henrique Bastos <henrique@bastos.net>
2017-06-05 19:33:31 -0300

    SubscriptionModelAdminTest.test_message

--- eventex/subscriptions/tests/test_admin.py
+++ eventex/subscriptions/tests/test_admin.py
@@ -1,3 +1,4 @@
+from unittest.mock import Mock
 from django.test import TestCase
 from eventex.subscriptions.admin import SubscriptionModelAdmin, Subscription, admin
 
@@ -16,6 +17,30 @@ class SubscriptionModelAdminTest(TestCase):
 
 		queryset = Subscription.objects.all()
 
+		mock = Mock()
+		old_message_user = SubscriptionModelAdmin.message_user
+		SubscriptionModelAdmin.message_user = mock
+
 		model_admin.mark_as_paid(None, queryset)
 
 		self.assertEqual(1, Subscription.objects.filter(paid=True).count())
+
+		SubscriptionModelAdmin.message_user = old_message_user
+
+	def test_message(self):
+		"""It should send a message to the user."""
+		Subscription.objects.create(name='Henrique Bastos', cpf='12345678901',
+			                        email='henrique@bastos.net', phone='21-99618-6180')
+		model_admin = SubscriptionModelAdmin(Subscription, admin.site)
+
+		queryset = Subscription.objects.all()
+
+		mock = Mock()
+		old_message_user = SubscriptionModelAdmin.message_user
+		SubscriptionModelAdmin.message_user = mock
+
+		model_admin.mark_as_paid(None, queryset)
+
+		mock.assert_called_once_with(None, '1 inscrição foi marcada como paga.')
+
+		SubscriptionModelAdmin.message_user = old_message_user
