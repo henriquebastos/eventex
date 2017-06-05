c33baac393e308a7741c26fc52fc6eba0479a915
Henrique Bastos <henrique@bastos.net>
2017-06-05 11:49:11 -0300

    Refactor new create to be more linear

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -16,21 +16,21 @@ def subscribe(request):
 def create(request):
 	form = SubscriptionForm(request.POST)
 
-	if form.is_valid():
-		body = render_to_string('subscriptions/subscription_email.txt',
-			                     form.cleaned_data)
-
-		mail.send_mail('Confirmação de inscrição',
-					   body,
-					   'contato@eventex.com.br',
-					   ['contato@eventex.com.br', form.cleaned_data['email']])
-
-		messages.success(request, 'Inscrição realizada com sucesso!')
-
-		return HttpResponseRedirect('/inscricao/')
-	else:
+	if not form.is_valid():
 		return render(request, 'subscriptions/subscription_form.html',
 			          {'form': form})
+
+	body = render_to_string('subscriptions/subscription_email.txt',
+		                     form.cleaned_data)
+
+	mail.send_mail('Confirmação de inscrição',
+				   body,
+				   'contato@eventex.com.br',
+				   ['contato@eventex.com.br', form.cleaned_data['email']])
+
+	messages.success(request, 'Inscrição realizada com sucesso!')
+
+	return HttpResponseRedirect('/inscricao/')
 
 
 def new(request):
