941c576fa63a2ecaf46b02c6f0477040de178d5c
Henrique Bastos <henrique@bastos.net>
2017-06-05 13:46:43 -0300

    Subscription fields verbose names

--- eventex/subscriptions/models.py
+++ eventex/subscriptions/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 
 class Subscription(models.Model):
-	name = models.CharField(max_length=100)
-	cpf = models.CharField(max_length=11)
-	email = models.EmailField()
-	phone = models.CharField(max_length=20)
-	created_at = models.DateTimeField(auto_now_add=True)
+	name = models.CharField('nome', max_length=100)
+	cpf = models.CharField('CPF', max_length=11)
+	email = models.EmailField('e-mail')
+	phone = models.CharField('telefone', max_length=20)
+	created_at = models.DateTimeField('criado em', auto_now_add=True)
 
 	class Meta:
 		verbose_name_plural = 'inscrições'
