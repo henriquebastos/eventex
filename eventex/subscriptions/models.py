58d9d609b17de6dd83834e5f9e1a4f0de8630d13
Henrique Bastos <henrique@bastos.net>
2017-06-05 19:04:13 -0300

    Subscription.paid verbose name

--- eventex/subscriptions/models.py
+++ eventex/subscriptions/models.py
@@ -7,7 +7,7 @@ class Subscription(models.Model):
 	email = models.EmailField('e-mail')
 	phone = models.CharField('telefone', max_length=20)
 	created_at = models.DateTimeField('criado em', auto_now_add=True)
-	paid = models.BooleanField(default=False)
+	paid = models.BooleanField('pago', default=False)
 
 	class Meta:
 		verbose_name_plural = 'inscrições'
