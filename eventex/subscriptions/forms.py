16c996c8cd955bd547a8e16d8883efc9f82499a2
Henrique Bastos <henrique@bastos.net>
2017-06-05 09:56:33 -0300

    Define label for SubscriptionForm fields

--- eventex/subscriptions/forms.py
+++ eventex/subscriptions/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 
 class SubscriptionForm(forms.Form):
-	name = forms.CharField()
-	cpf = forms.CharField()
-	email = forms.EmailField()
-	phone = forms.CharField()
+	name = forms.CharField(label='Nome')
+	cpf = forms.CharField(label='CPF')
+	email = forms.EmailField(label='Email')
+	phone = forms.CharField(label='Telefone')
