deb636b39a744bee48c3e053d1b1c4f0d06ce642
Henrique Bastos <henrique@bastos.net>
2017-06-05 11:47:18 -0300

    Split subscribe into create and new subviews

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -8,23 +8,31 @@ from eventex.subscriptions.forms import SubscriptionForm
 
 def subscribe(request):
 	if request.method == 'POST':
-		form = SubscriptionForm(request.POST)
-
-		if form.is_valid():
-			body = render_to_string('subscriptions/subscription_email.txt',
-				                     form.cleaned_data)
-
-			mail.send_mail('Confirmação de inscrição',
-						   body,
-						   'contato@eventex.com.br',
-						   ['contato@eventex.com.br', form.cleaned_data['email']])
-
-			messages.success(request, 'Inscrição realizada com sucesso!')
-
-			return HttpResponseRedirect('/inscricao/')
-		else:
-			return render(request, 'subscriptions/subscription_form.html',
-				          {'form': form})
+		return create(request)
 	else:
-		context = {'form': SubscriptionForm()}
-		return render(request, 'subscriptions/subscription_form.html', context)
+		return new(request)
+
+
+def create(request):
+	form = SubscriptionForm(request.POST)
+
+	if form.is_valid():
+		body = render_to_string('subscriptions/subscription_email.txt',
+			                     form.cleaned_data)
+
+		mail.send_mail('Confirmação de inscrição',
+					   body,
+					   'contato@eventex.com.br',
+					   ['contato@eventex.com.br', form.cleaned_data['email']])
+
+		messages.success(request, 'Inscrição realizada com sucesso!')
+
+		return HttpResponseRedirect('/inscricao/')
+	else:
+		return render(request, 'subscriptions/subscription_form.html',
+			          {'form': form})
+
+
+def new(request):
+	context = {'form': SubscriptionForm()}
+	return render(request, 'subscriptions/subscription_form.html', context)
