0a3d755d9ea11c5b130535ad12a6e58a7936471a
Henrique Bastos <henrique@bastos.net>
2017-06-05 19:01:51 -0300

    SubscriptionModelAdmin.list_filter

--- eventex/subscriptions/admin.py
+++ eventex/subscriptions/admin.py
@@ -8,6 +8,7 @@ class SubscriptionModelAdmin(admin.ModelAdmin):
 		            'subscribed_today')
 	date_hierarchy = 'created_at'
 	search_fields = ('name', 'email', 'phone', 'cpf', 'created_at')
+	list_filter = ('created_at',)
 
 	def subscribed_today(self, obj):
 		return obj.created_at.date() == now().date()
