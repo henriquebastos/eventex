e59038c23724246a7cf4d5c94d472a5ef02ea4b2
Henrique Bastos <henrique@bastos.net>
2017-06-05 13:24:39 -0300

    SubscriptionModelTest.test_create

---
+++ eventex/subscriptions/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+
+class Subscription(models.Model):
+	name = models.CharField(max_length=100)
+	cpf = models.CharField(max_length=11)
+	email = models.EmailField()
+	phone = models.CharField(max_length=20)
