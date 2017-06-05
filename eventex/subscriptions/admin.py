657ff36d412b11788ef96638875d193ac815fab1
Henrique Bastos <henrique@bastos.net>
2017-06-05 13:49:29 -0300

    SubscriptionModelAdmin.date_hierarchy

--- eventex/subscriptions/admin.py
+++ eventex/subscriptions/admin.py
@@ -4,6 +4,7 @@ from eventex.subscriptions.models import Subscription
 
 class SubscriptionModelAdmin(admin.ModelAdmin):
 	list_display = ('name', 'email', 'phone', 'cpf', 'created_at')
+	date_hierarchy = 'created_at'
 
 
 admin.site.register(Subscription, SubscriptionModelAdmin)
