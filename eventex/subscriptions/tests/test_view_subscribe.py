132939f3ffffcee174ad866e492aaf23b701ead4
Henrique Bastos <henrique@bastos.net>
2017-06-05 13:33:19 -0300

    SubscriptionPostValid.test_save_subscription

--- eventex/subscriptions/tests/test_view_subscribe.py
+++ eventex/subscriptions/tests/test_view_subscribe.py
@@ -1,6 +1,7 @@
 from django.core import mail
 from django.test import TestCase
 from eventex.subscriptions.forms import SubscriptionForm
+from eventex.subscriptions.models import Subscription
 
 
 class SubscribeGet(TestCase):
@@ -50,6 +51,9 @@ class SubscribePostValid(TestCase):
 	def test_send_subscribe_email(self):
 		self.assertEqual(1, len(mail.outbox))
 
+	def test_save_subscription(self):
+		self.assertTrue(Subscription.objects.exists())
+
 
 class SubscribePostInvalid(TestCase):
 	def setUp(self):
@@ -69,6 +73,9 @@ class SubscribePostInvalid(TestCase):
 	def test_form_has_errors(self):
 		form = self.resp.context['form']
 		self.assertTrue(form.errors)
+
+	def test_dont_save_subscription(self):
+		self.assertFalse(Subscription.objects.exists())
 
 
 class SubscribeSuccessMessage(TestCase):
