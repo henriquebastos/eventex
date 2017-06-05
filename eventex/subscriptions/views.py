74786c6300d9e5d0d1e7b69b68521537f14e1c53
Henrique Bastos <henrique@bastos.net>
2017-06-05 10:18:52 -0300

    Refactor email body to a template file

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -1,13 +1,19 @@
 from django.core import mail
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.template.loader import render_to_string
 from eventex.subscriptions.forms import SubscriptionForm
 
 
 def subscribe(request):
 	if request.method == 'POST':
+		context = dict(name='Henrique Bastos', cpf='12345678901',
+			           email='henrique@bastos.net', phone='21-99618-6180')
+		body = render_to_string('subscriptions/subscription_email.txt',
+			                     context)
+
 		mail.send_mail('Confirmação de inscrição',
-					   MESSAGE,
+					   body,
 					   'contato@eventex.com.br',
 					   ['contato@eventex.com.br', 'henrique@bastos.net'])
 
@@ -15,26 +21,3 @@ def subscribe(request):
 	else:
 		context = {'form': SubscriptionForm()}
 		return render(request, 'subscriptions/subscription_form.html', context)
-
-
-MESSAGE = """
-Olá! Tudo bem?
-
-Muito obrigada por se inscrever no Eventex.
-
-Estes foram os dados que você nos forneceu
-em sua inscrição:
-
-Nome: Henrique Bastos
-CPF: 12345678901
-Email: henrique@bastos.net
-Telefone: 21-99618-6180
-
-Em até 48 horas úteis, a nossa equipe entrará
-em contato com vocie para concluirmos a sua
-matrícula.
-
-Atenciosamente,
---
-Morena
-"""
