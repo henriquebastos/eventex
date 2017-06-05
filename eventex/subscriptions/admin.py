a9628529633cf3393563cfedd75a079877eae3dd
Henrique Bastos <henrique@bastos.net>
2017-06-05 19:03:50 -0300

    Add Subscription.paid to ModelAdmin

--- eventex/subscriptions/admin.py
+++ eventex/subscriptions/admin.py
@@ -5,10 +5,10 @@ from eventex.subscriptions.models import Subscription
 
 class SubscriptionModelAdmin(admin.ModelAdmin):
 	list_display = ('name', 'email', 'phone', 'cpf', 'created_at',
-		            'subscribed_today')
+		            'subscribed_today', 'paid')
 	date_hierarchy = 'created_at'
 	search_fields = ('name', 'email', 'phone', 'cpf', 'created_at')
-	list_filter = ('created_at',)
+	list_filter = ('paid', 'created_at')
 
 	def subscribed_today(self, obj):
 		return obj.created_at.date() == now().date()
