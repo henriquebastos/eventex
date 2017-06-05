35c9f4804799bb805eb3839177283a2acb5d890c
Henrique Bastos <henrique@bastos.net>
2017-06-05 10:15:07 -0300

    SubscribePostTest.test_subscription_email_body

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -7,7 +7,7 @@ from eventex.subscriptions.forms import SubscriptionForm
 def subscribe(request):
 	if request.method == 'POST':
 		mail.send_mail('Confirmação de inscrição',
-					   'Message',
+					   MESSAGE,
 					   'contato@eventex.com.br',
 					   ['contato@eventex.com.br', 'henrique@bastos.net'])
 
@@ -15,3 +15,26 @@ def subscribe(request):
 	else:
 		context = {'form': SubscriptionForm()}
 		return render(request, 'subscriptions/subscription_form.html', context)
+
+
+MESSAGE = """
+Olá! Tudo bem?
+
+Muito obrigada por se inscrever no Eventex.
+
+Estes foram os dados que você nos forneceu
+em sua inscrição:
+
+Nome: Henrique Bastos
+CPF: 12345678901
+Email: henrique@bastos.net
+Telefone: 21-99618-6180
+
+Em até 48 horas úteis, a nossa equipe entrará
+em contato com vocie para concluirmos a sua
+matrícula.
+
+Atenciosamente,
+--
+Morena
+"""
