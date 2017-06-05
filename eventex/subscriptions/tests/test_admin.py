19eb7b3cbf09ae3eb62cc3691f29a44b35dc013e
Henrique Bastos <henrique@bastos.net>
2017-06-05 19:35:01 -0300

    SubscriptionModelAdminTest.setUp

--- eventex/subscriptions/tests/test_admin.py
+++ eventex/subscriptions/tests/test_admin.py
@@ -4,24 +4,24 @@ from eventex.subscriptions.admin import SubscriptionModelAdmin, Subscription, ad
 
 
 class SubscriptionModelAdminTest(TestCase):
+	def setUp(self):
+		Subscription.objects.create(name='Henrique Bastos', cpf='12345678901',
+			                        email='henrique@bastos.net', phone='21-99618-6180')
+		self.model_admin = SubscriptionModelAdmin(Subscription, admin.site)
+
 	def test_has_action(self):
 		"""Action mark_as_paid should be installed."""
-		model_admin = SubscriptionModelAdmin(Subscription, admin.site)
-		self.assertIn('mark_as_paid', model_admin.actions)
+		self.assertIn('mark_as_paid', self.model_admin.actions)
 
 	def test_mark_all(self):
 		"""It should mark all selected subscriptions as paid."""
-		Subscription.objects.create(name='Henrique Bastos', cpf='12345678901',
-			                        email='henrique@bastos.net', phone='21-99618-6180')
-		model_admin = SubscriptionModelAdmin(Subscription, admin.site)
-
 		queryset = Subscription.objects.all()
 
 		mock = Mock()
 		old_message_user = SubscriptionModelAdmin.message_user
 		SubscriptionModelAdmin.message_user = mock
 
-		model_admin.mark_as_paid(None, queryset)
+		self.model_admin.mark_as_paid(None, queryset)
 
 		self.assertEqual(1, Subscription.objects.filter(paid=True).count())
 
@@ -29,17 +29,13 @@ class SubscriptionModelAdminTest(TestCase):
 
 	def test_message(self):
 		"""It should send a message to the user."""
-		Subscription.objects.create(name='Henrique Bastos', cpf='12345678901',
-			                        email='henrique@bastos.net', phone='21-99618-6180')
-		model_admin = SubscriptionModelAdmin(Subscription, admin.site)
-
 		queryset = Subscription.objects.all()
 
 		mock = Mock()
 		old_message_user = SubscriptionModelAdmin.message_user
 		SubscriptionModelAdmin.message_user = mock
 
-		model_admin.mark_as_paid(None, queryset)
+		self.model_admin.mark_as_paid(None, queryset)
 
 		mock.assert_called_once_with(None, '1 inscrição foi marcada como paga.')
 
