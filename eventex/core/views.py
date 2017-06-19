14ba78d6194b8ea00eb4291fa9ff2560d92a8bf5
Henrique Bastos <henrique@bastos.net>
2017-06-19 18:06:32 -0300

    HomeTest with fixtures

--- eventex/core/views.py
+++ eventex/core/views.py
@@ -2,10 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from eventex.core.models import Speaker
 
 def home(request):
-	speakers = [
-		{'name': 'Grace Hopper', 'photo': 'http://hbn.link/hopper-pic'},
-		{'name': 'Alan Turing', 'photo': 'http://hbn.link/turing-pic'},
-	]
+	speakers = Speaker.objects.all()
 	return render(request, 'index.html', {'speakers': speakers})
 
 
