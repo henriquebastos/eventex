cf1fc8cf4e4aafbd72aed7bd62d81412b6c82bac
Henrique Bastos <henrique@bastos.net>
2017-06-19 12:51:11 -0300

    SubscriptionFormTest.make_validated_form

--- eventex/subscriptions/tests/test_form_subscription.py
+++ eventex/subscriptions/tests/test_form_subscription.py
@@ -13,19 +13,18 @@ class SubscriptionFormTest(TestCase):
 
 	def test_cpf_is_digit(self):
 		"""CPF must only accept digits."""
-		data = dict(name='Henrique Bastos', cpf='ABCD5678901',
-			        email='henrique@bastos.net', phone='21-996186180')
-		form = SubscriptionForm(data)
-		form.is_valid()
-
+		form = self.make_validated_form(cpf='ABCD5678901')
 		self.assertListEqual(['cpf'], list(form.errors))
 
 	def test_cpf_has_11_digits(self):
 		"""CPF must have 11 digits"""
-		data = dict(name='Henrique Bastos', cpf='1234',
-			        email='henrique@bastos.net', phone='21-996186180')
-		form = SubscriptionForm(data)
-		form.is_valid()
-
+		form = self.make_validated_form(cpf='1234')
 		self.assertListEqual(['cpf'], list(form.errors))
 
+	def make_validated_form(self, **kwargs):
+		valid = dict(name='Henrique Bastos', cpf='12345678901',
+			         email='henrique@bastos.net', phone='21-996186180')
+		data = dict(valid, **kwargs)
+		form = SubscriptionForm(data)
+		form.is_valid()
+		return form
