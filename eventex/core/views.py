95ba7c2e4c90cd843b30122c4eb88f884c3ff8a1
Henrique Bastos <henrique@bastos.net>
2017-06-19 16:49:21 -0300

    Speaker data via context

--- eventex/core/views.py
+++ eventex/core/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'index.html')
+	speakers = [
+		{'name': 'Grace Hopper', 'photo': 'http://hbn.link/hopper-pic'},
+		{'name': 'Alan Turing', 'photo': 'http://hbn.link/turing-pic'},
+	]
+	return render(request, 'index.html', {'speakers': speakers})
