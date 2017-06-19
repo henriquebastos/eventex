7b962a189edc41455dc9b6c2e87aca7d5b49e44a
Henrique Bastos <henrique@bastos.net>
2017-06-19 13:02:09 -0300

    SubscriptionFormTest.assertFormErrorCode

--- eventex/subscriptions/forms.py
+++ eventex/subscriptions/forms.py
@@ -4,10 +4,10 @@ from django.core.exceptions import ValidationError
 
 def validate_cpf(value):
 	if not value.isdigit():
-		raise ValidationError('CPF deve conter apenas números.')
+		raise ValidationError('CPF deve conter apenas números.', 'digits')
 
 	if len(value) != 11:
-		raise ValidationError('CPF deve ter 11 números.')
+		raise ValidationError('CPF deve ter 11 números.', 'length')
 
 
 class SubscriptionForm(forms.Form):
