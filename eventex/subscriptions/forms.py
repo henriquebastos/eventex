56dcc4f767e169bf146bab7a609890eb71b86fdc
Henrique Bastos <henrique@bastos.net>
2017-06-19 13:12:46 -0300

    SubscriptionFormTest.test_phone_is_optional

--- eventex/subscriptions/forms.py
+++ eventex/subscriptions/forms.py
@@ -14,7 +14,7 @@ class SubscriptionForm(forms.Form):
 	name = forms.CharField(label='Nome')
 	cpf = forms.CharField(label='CPF', validators=[validate_cpf])
 	email = forms.EmailField(label='Email', required=False)
-	phone = forms.CharField(label='Telefone')
+	phone = forms.CharField(label='Telefone', required=False)
 
 	def clean_name(self):
 		name = self.cleaned_data['name']
