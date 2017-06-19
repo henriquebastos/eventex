274b92e1ae239cf4d0c78d7cac4a288a2e79176a
Henrique Bastos <henrique@bastos.net>
2017-06-19 16:19:47 -0300

    Subscription Model email and phone blank=True

--- eventex/subscriptions/models.py
+++ eventex/subscriptions/models.py
@@ -5,8 +5,8 @@ from eventex.subscriptions.validators import validate_cpf
 class Subscription(models.Model):
 	name = models.CharField('nome', max_length=100)
 	cpf = models.CharField('CPF', max_length=11, validators=[validate_cpf])
-	email = models.EmailField('e-mail')
-	phone = models.CharField('telefone', max_length=20)
+	email = models.EmailField('e-mail', blank=True)
+	phone = models.CharField('telefone', max_length=20, blank=True)
 	created_at = models.DateTimeField('criado em', auto_now_add=True)
 	paid = models.BooleanField('pago', default=False)
 
