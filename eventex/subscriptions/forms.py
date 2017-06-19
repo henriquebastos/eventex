399b9fa311c4039edc1c4ae881391eacf43f0f5f
Henrique Bastos <henrique@bastos.net>
2017-06-19 16:09:48 -0300

    ModelForm replace cpf field

--- eventex/subscriptions/forms.py
+++ eventex/subscriptions/forms.py
@@ -4,32 +4,34 @@ from eventex.subscriptions.models import Subscription
 
 
 def validate_cpf(value):
-	if not value.isdigit():
-		raise ValidationError('CPF deve conter apenas números.', 'digits')
+    if not value.isdigit():
+        raise ValidationError('CPF deve conter apenas números.', 'digits')
 
-	if len(value) != 11:
-		raise ValidationError('CPF deve ter 11 números.', 'length')
+    if len(value) != 11:
+        raise ValidationError('CPF deve ter 11 números.', 'length')
 
 
 class SubscriptionFormOld(forms.Form):
-	name = forms.CharField(label='Nome')
-	cpf = forms.CharField(label='CPF', validators=[validate_cpf])
-	email = forms.EmailField(label='Email', required=False)
-	phone = forms.CharField(label='Telefone', required=False)
+    name = forms.CharField(label='Nome')
+    cpf = forms.CharField(label='CPF', validators=[validate_cpf])
+    email = forms.EmailField(label='Email', required=False)
+    phone = forms.CharField(label='Telefone', required=False)
 
-	def clean_name(self):
-		name = self.cleaned_data['name']
-		words = [w.capitalize() for w in name.split()]
-		return ' '.join(words)
+    def clean_name(self):
+        name = self.cleaned_data['name']
+        words = [w.capitalize() for w in name.split()]
+        return ' '.join(words)
 
-	def clean(self):
-		if not self.cleaned_data.get('email') and not self.cleaned_data.get('phone'):
-			raise ValidationError('Informe seu e-mail ou telefone.')
+    def clean(self):
+        if not self.cleaned_data.get('email') and not self.cleaned_data.get('phone'):
+            raise ValidationError('Informe seu e-mail ou telefone.')
 
-		return self.cleaned_data
+        return self.cleaned_data
 
 
 class SubscriptionForm(forms.ModelForm):
-	class Meta:
-		model = Subscription
-		fields = ['name', 'cpf', 'email', 'phone']
+    cpf = forms.CharField(label='CPF', validators=[validate_cpf])
+
+    class Meta:
+        model = Subscription
+        fields = ['name', 'cpf', 'email', 'phone']
