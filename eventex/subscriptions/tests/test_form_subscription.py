d8de1ccbe3af910c737eac988391d7bdc48da3ab
Henrique Bastos <henrique@bastos.net>
2017-06-19 13:06:39 -0300

    SubscriptionFormTest.test_name_must_be_capitalized

--- eventex/subscriptions/tests/test_form_subscription.py
+++ eventex/subscriptions/tests/test_form_subscription.py
@@ -19,6 +19,11 @@ class SubscriptionFormTest(TestCase):
 		form = self.make_validated_form(cpf='1234')
 		self.assertFormErrorCode(form, 'cpf', 'length')
 
+	def test_name_must_be_capitalized(self):
+		"""Name must be capitalized."""
+		form = self.make_validated_form(name='HENRIQUE bastos')
+		self.assertEqual('Henrique Bastos', form.cleaned_data['name'])
+
 	def assertFormErrorCode(self, form, field, code):
 		errors = form.errors.as_data()
 		errors_list = errors[field]
