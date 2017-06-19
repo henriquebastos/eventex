3c9c540676a24cfa846f136efbfdedef6e191a7d
Henrique Bastos <henrique@bastos.net>
2017-06-19 13:18:26 -0300

    dict.get on SubscriptionForm.clean

--- eventex/subscriptions/forms.py
+++ eventex/subscriptions/forms.py
@@ -22,7 +22,7 @@ class SubscriptionForm(forms.Form):
 		return ' '.join(words)
 
 	def clean(self):
-		if not self.cleaned_data['email'] and not self.cleaned_data['phone']:
+		if not self.cleaned_data.get('email') and not self.cleaned_data.get('phone'):
 			raise ValidationError('Informe seu e-mail ou telefone.')
 
 		return self.cleaned_data
