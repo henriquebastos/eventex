987b552e01c26dcc8af51f43c26dd83b7a6ad8cd
Henrique Bastos <henrique@bastos.net>
2017-06-19 13:11:55 -0300

    SubscriptionFormTest.test_email_is_optional

--- eventex/subscriptions/tests/test_form_subscription.py
+++ eventex/subscriptions/tests/test_form_subscription.py
@@ -24,6 +24,11 @@ class SubscriptionFormTest(TestCase):
 		form = self.make_validated_form(name='HENRIQUE bastos')
 		self.assertEqual('Henrique Bastos', form.cleaned_data['name'])
 
+	def test_email_is_optional(self):
+		"""Email is optional."""
+		form = self.make_validated_form(email='')
+		self.assertFalse(form.errors)
+
 	def assertFormErrorCode(self, form, field, code):
 		errors = form.errors.as_data()
 		errors_list = errors[field]
