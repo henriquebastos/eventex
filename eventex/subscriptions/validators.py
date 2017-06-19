83f79440199877029d9f9bead5a698e0ab2ff8b9
Henrique Bastos <henrique@bastos.net>
2017-06-19 16:13:09 -0300

    validators.py

---
+++ eventex/subscriptions/validators.py
@@ -0,0 +1,9 @@
+from django.core.exceptions import ValidationError
+
+
+def validate_cpf(value):
+    if not value.isdigit():
+        raise ValidationError('CPF deve conter apenas números.', 'digits')
+
+    if len(value) != 11:
+        raise ValidationError('CPF deve ter 11 números.', 'length')
