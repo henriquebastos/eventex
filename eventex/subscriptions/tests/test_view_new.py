436e270636fef61f9ac9ee38375203eacf4f95b1
Henrique Bastos <henrique@bastos.net>
2017-06-19 13:21:57 -0300

    TemplateRegressionTest form.non_field_errors

--- eventex/subscriptions/tests/test_view_new.py
+++ eventex/subscriptions/tests/test_view_new.py
@@ -77,3 +77,12 @@ class SubscriptionsNewPostInvalid(TestCase):
 
 	def test_dont_save_subscription(self):
 		self.assertFalse(Subscription.objects.exists())
+
+
+class TemplateRegressionTest(TestCase):
+	def test_template_has_non_field_errors(self):
+		invalid_data = dict(name='Henrique Bastos', cpf='12345678901')
+		response = self.client.post(r('subscriptions:new'), invalid_data)
+
+		self.assertContains(response, '<ul class="errorlist nonfield">')
+
