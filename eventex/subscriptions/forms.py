e0cacf1725697c40c0d303909ca41c5c1475b2ac
Henrique Bastos <henrique@bastos.net>
2017-06-19 16:20:50 -0300

    ModelForm clean_name

--- eventex/subscriptions/forms.py
+++ eventex/subscriptions/forms.py
@@ -10,11 +10,6 @@ class SubscriptionFormOld(forms.Form):
     email = forms.EmailField(label='Email', required=False)
     phone = forms.CharField(label='Telefone', required=False)
 
-    def clean_name(self):
-        name = self.cleaned_data['name']
-        words = [w.capitalize() for w in name.split()]
-        return ' '.join(words)
-
     def clean(self):
         if not self.cleaned_data.get('email') and not self.cleaned_data.get('phone'):
             raise ValidationError('Informe seu e-mail ou telefone.')
@@ -26,3 +21,8 @@ class SubscriptionForm(forms.ModelForm):
     class Meta:
         model = Subscription
         fields = ['name', 'cpf', 'email', 'phone']
+
+    def clean_name(self):
+        name = self.cleaned_data['name']
+        words = [w.capitalize() for w in name.split()]
+        return ' '.join(words)
