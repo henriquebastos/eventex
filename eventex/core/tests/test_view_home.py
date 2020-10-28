e994f16d0524c3a486cf334e96530affc75826ff
Giandoso <a15029@bcc.unifal-mg.edu.br>
2020-10-28 08:45:10 -0300

    Refactor HomeTest.Setup with resolve_url

--- eventex/core/tests/test_view_home.py
+++ eventex/core/tests/test_view_home.py
@@ -1,9 +1,10 @@
 from django.test import TestCase
+from django.shortcuts import resolve_url as r
 
 
 class HomeTest(TestCase):
 	def setUp(self):
-		self.response = self.client.get('/')
+		self.response = self.client.get(r('home'))
 
 	def test_get(self):
 		"""GET / must return status code 200"""
