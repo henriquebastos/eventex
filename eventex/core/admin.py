1ff619f0e104856613fb0f272b25ea014b6a046e
Giandoso <a15029@bcc.unifal-mg.edu.br>
2020-10-29 12:00:42 -0300

    list website_link

--- eventex/core/admin.py
+++ eventex/core/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 from eventex.core.models import Speaker
 
 
 class SpeakerModelAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
+    list_display = ['name', 'website_link']
 
+    def website_link(self, obj):
+        return format_html('<a href="{0}">{0}</a>', obj.website)
+
+    website_link.short_description = 'website'
 
 admin.site.register(Speaker, SpeakerModelAdmin)
