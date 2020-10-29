579d9b2be7fd101770f2e090b5ec4493babeba10
Giandoso <a15029@bcc.unifal-mg.edu.br>
2020-10-29 11:41:00 -0300

    Speaker verbose_name

--- eventex/core/models.py
+++ eventex/core/models.py
@@ -2,8 +2,12 @@ from django.db import models
 
 
 class Speaker(models.Model):
-    name = models.CharField(max_length=255)
-    slug = models.SlugField()
-    photo = models.URLField()
-    website = models.URLField()
-    description = models.TextField()
+    name = models.CharField('nome', max_length=255)
+    slug = models.SlugField('slug')
+    photo = models.URLField('foto')
+    website = models.URLField('website')
+    description = models.TextField('descrição')
+
+    class Meta:
+        verbose_name = 'palestrante'
+        verbose_name_plural = 'palestrantes'
