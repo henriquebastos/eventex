812b390928a582e89a26606dcbe8c465568d49fd
Giandoso <a15029@bcc.unifal-mg.edu.br>
2020-10-29 12:03:35 -0300

    list photo_img

--- eventex/core/admin.py
+++ eventex/core/admin.py
@@ -6,11 +6,16 @@ from eventex.core.models import Speaker
 
 class SpeakerModelAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
-    list_display = ['name', 'website_link']
+    list_display = ['name', 'photo_img', 'website_link']
 
     def website_link(self, obj):
         return format_html('<a href="{0}">{0}</a>', obj.website)
 
     website_link.short_description = 'website'
+
+    def photo_img(self, obj):
+        return format_html('<img width="32px" src="{}"/>', obj.photo)
+
+    photo_img.short_description = 'foto'
 
 admin.site.register(Speaker, SpeakerModelAdmin)
