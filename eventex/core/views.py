903f7125c4b9502ec161fd816edda0d1ac69fa9e
Giandoso <a15029@bcc.unifal-mg.edu.br>
2020-10-29 11:13:23 -0300

    SpeakerDetailNotFound.test_not_found

--- eventex/core/views.py
+++ eventex/core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from eventex.core.models import Speaker
 
 def home(request):
@@ -10,6 +10,6 @@ def home(request):
 
 
 def speaker_detail(request, slug):
-	speaker = Speaker.objects.get(slug=slug)
+	speaker = get_object_or_404(Speaker, slug=slug)
 	return render(request, 'core/speaker_detail.html', {'speaker': speaker})
 
