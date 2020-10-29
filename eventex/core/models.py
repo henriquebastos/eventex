fa8a118ae1b88126721ecb421cc8f70d3fab4f54
Giandoso <a15029@bcc.unifal-mg.edu.br>
2020-10-29 10:17:00 -0300

    Refactor use template tags in speaker_detail.html

--- eventex/core/models.py
+++ eventex/core/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
 
-class Speaker():
-    pass
+class Speaker:
+    name = 'Grace Hopper'
+    website = 'http://hbn.link/hopper-site'
+    photo = 'http://hbn.link/hopper-pic'
+    description = 'Programadora e almirante.'
