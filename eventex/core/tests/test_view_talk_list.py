a0b2ece771961e43a753c3e236926c9f876b8ec9
Giandoso <a15029@bcc.unifal-mg.edu.br>
2020-10-30 07:39:45 -0300

    TalkListGet.setUp

--- eventex/core/tests/test_view_talk_list.py
+++ eventex/core/tests/test_view_talk_list.py
@@ -3,6 +3,9 @@ from django.shortcuts import resolve_url as r
 
 
 class TalkListGet(TestCase):
+    def setUp(self):
+        self.resp = self.client.get(r('talk_list'))
+
     def test_get(self):
-        resp = self.client.get(r('talk_list'))
-        self.assertEqual(200, resp.status_code)
+        self.assertEqual(200, self.resp.status_code)
+
