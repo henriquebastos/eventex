2bfef46d9bba13da2e9f35911c1b045b5bc2930c
Giandoso <a15029@bcc.unifal-mg.edu.br>
2020-10-30 09:39:29 -0300

    TalkModelTest.test_has_speakers

--- eventex/core/models.py
+++ eventex/core/models.py
@@ -45,3 +45,4 @@ class Talk(models.Model):
     title = models.CharField(max_length=200)
     start = models.TimeField()
     description = models.TextField()
+    speakers = models.ManyToManyField('Speaker')
