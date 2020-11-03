96de1722e7517601614404ca82ad74af6e19bea2
Giandoso <a15029@bcc.unifal-mg.edu.br>
2020-11-03 17:21:48 -0300

    Refactor abstract model Activity to Talk

--- eventex/core/models.py
+++ eventex/core/models.py
@@ -45,7 +45,7 @@ class Contact(models.Model):
         return self.value
 
 
-class Activity(models.Model):
+class Talk(models.Model):
     title = models.CharField('título', max_length=200)
     start = models.TimeField('início', blank=True, null=True)
     description = models.TextField('descrição', blank=True)
@@ -54,7 +54,6 @@ class Activity(models.Model):
     objects = PeriodManager()
 
     class Meta:
-        abstract = True
         ordering = ['start']
         verbose_name = 'palestra'
         verbose_name_plural = 'palestras'
@@ -63,12 +62,10 @@ class Activity(models.Model):
         return self.title
 
 
-class Talk(Activity):
-    pass
-
-
 class Course(Talk):
     slots = models.IntegerField()
+
+    objects = PeriodManager()
 
     class Meta:
         verbose_name = 'curso'
