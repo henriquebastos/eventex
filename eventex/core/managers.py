8a147ec0351ad4464084a2cc884b6019a4bf24b1
Giandoso <a15029@bcc.unifal-mg.edu.br>
2020-11-03 10:19:24 -0300

    Refactor use custom queryset

--- eventex/core/managers.py
+++ eventex/core/managers.py
@@ -1,9 +1,20 @@
 from django.db import models
 
 
-class KindContactManager(models.Manager):
+class KindQuerySet(models.QuerySet):
     def emails(self):
         return self.filter(kind=self.model.EMAIL)
 
     def phones(self):
         return self.filter(kind=self.model.PHONE)
+
+
+class KindContactManager(models.Manager):
+    def get_queryset(self):
+        return KindQuerySet(self.model, using=self._db)
+
+    def emails(self):
+        return self.get_queryset().emails()
+
+    def phones(self):
+        return self.get_queryset().phones()
