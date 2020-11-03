71466a265b5e9e41af05e56f6893f2749929c19a
Giandoso <a15029@bcc.unifal-mg.edu.br>
2020-11-03 11:46:51 -0300

    Refactor get Course from db

--- eventex/core/views.py
+++ eventex/core/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from eventex.core.models import Speaker, Talk
+from eventex.core.models import Speaker, Talk, Course
 
 
 def home(request):
@@ -13,17 +13,9 @@ def speaker_detail(request, slug):
 
 
 def talk_list(request):
-    speaker = Speaker(name='Henrique Bastos', slug='henrique-bastos')
-    courses = [
-        dict(title='Título do Curso',
-             start='09:00',
-             description='Descrição do curso.',
-             speakers={'all': [speaker]})
-
-    ]
     context = {
         'morning_talks': Talk.objects.at_morning(),
         'afternoon_talks': Talk.objects.at_afternoon(),
-        'courses': courses,
+        'courses': Course.objects.all,
     }
     return render(request, 'core/talk_list.html', context=context)
