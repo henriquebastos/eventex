149809b9a45aed6507d9bba803adbde870b9945d
Giandoso <a15029@bcc.unifal-mg.edu.br>
2020-11-03 11:07:44 -0300

    Refactor pass courses via context

--- eventex/core/views.py
+++ eventex/core/views.py
@@ -13,8 +13,17 @@ def speaker_detail(request, slug):
 
 
 def talk_list(request):
+    speaker = Speaker(name='Henrique Bastos', slug='henrique-bastos')
+    courses = [
+        dict(title='Título do Curso',
+             start='09:00',
+             description='Descrição do curso.',
+             speakers={'all': [speaker]})
+
+    ]
     context = {
         'morning_talks': Talk.objects.at_morning(),
         'afternoon_talks': Talk.objects.at_afternoon(),
+        'courses': courses,
     }
     return render(request, 'core/talk_list.html', context=context)
