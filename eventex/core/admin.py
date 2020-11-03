1255b42a036140c612616897a7ffa3d6fab78a30
Giandoso <a15029@bcc.unifal-mg.edu.br>
2020-11-03 17:24:58 -0300

    Refactor TalkModelAdmin

--- eventex/core/admin.py
+++ eventex/core/admin.py
@@ -35,6 +35,12 @@ class SpeakerModelAdmin(admin.ModelAdmin):
     phone.short_description = 'telefone'
 
 
+class TalkModelAdmin(admin.ModelAdmin):
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.filter(course=None)
+
+
 admin.site.register(Speaker, SpeakerModelAdmin)
-admin.site.register(Talk)
+admin.site.register(Talk, TalkModelAdmin)
 admin.site.register(Course)
