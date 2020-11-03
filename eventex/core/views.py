361da37fa863ddb4fcec00893ae356f6ac2325f0
Giandoso <a15029@bcc.unifal-mg.edu.br>
2020-11-03 10:46:37 -0300

    Refactor use PeriodManager in views

--- eventex/core/views.py
+++ eventex/core/views.py
@@ -14,7 +14,7 @@ def speaker_detail(request, slug):
 
 def talk_list(request):
     context = {
-        'morning_talks': Talk.objects.filter(start__lt='12:00'),
-        'afternoon_talks': Talk.objects.filter(start__gte='12:00'),
+        'morning_talks': Talk.objects.at_morning(),
+        'afternoon_talks': Talk.objects.at_afternoon(),
     }
     return render(request, 'core/talk_list.html', context=context)
