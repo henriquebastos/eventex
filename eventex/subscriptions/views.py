6260e4023ebcdb9f9f5877855e59587357be63eb
Giandoso <a15029@bcc.unifal-mg.edu.br>
2020-11-04 18:53:40 -0300

    Refactor detail view

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -4,6 +4,8 @@ from django.core import mail
 from django.http import HttpResponseRedirect, Http404
 from django.shortcuts import render, resolve_url as r
 from django.template.loader import render_to_string
+from django.views.generic import DetailView
+
 from eventex.subscriptions.forms import SubscriptionForm
 from eventex.subscriptions.models import Subscription
 
@@ -37,14 +39,7 @@ def create(request):
 	return HttpResponseRedirect(r('subscriptions:detail', subscription.pk))
 
 
-def detail(request, pk):
-	try:
-		subscription = Subscription.objects.get(pk=pk)
-	except Subscription.DoesNotExist:
-		raise Http404
-
-	return render(request, 'subscriptions/subscription_detail.html',
-		          {'subscription': subscription})
+detail = DetailView.as_view(model=Subscription)
 
 
 def _send_mail(subject, from_, to, template_name, context):
