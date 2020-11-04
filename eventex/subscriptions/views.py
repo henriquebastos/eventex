029b78ba14326be8423d1d60632c4923636d3ab1
Giandoso <a15029@bcc.unifal-mg.edu.br>
2020-11-04 19:45:10 -0300

    Refactor install BaseCreateView

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -4,26 +4,18 @@ from django.http import HttpResponseRedirect
 from django.template.loader import render_to_string
 from django.views.generic import DetailView
 from django.views.generic.base import TemplateResponseMixin
-from django.views.generic.edit import ModelFormMixin, ProcessFormView
+from django.views.generic.edit import BaseCreateView
 
 from eventex.subscriptions.forms import SubscriptionForm
 from eventex.subscriptions.models import Subscription
 
 
-class SubscriptionCreate(TemplateResponseMixin, ModelFormMixin, ProcessFormView):
+class SubscriptionCreate(TemplateResponseMixin, BaseCreateView):
     template_name = 'subscriptions/subscription_form.html'
     form_class = SubscriptionForm
 
-    def get(self, *args, **kwargs):
-        self.object = None
-        return super().get(*args, **kwargs)
-
-    def post(self, *args, **kwargs):
-        self.object = None
-        return super().post(*args, **kwargs)
-
     def form_valid(self, form):
-        self.object = form.save()
+        response = super().form_valid(form)
 
         _send_mail('Confirmação de inscrição',
                    settings.DEFAULT_FROM_EMAIL,
@@ -31,7 +23,7 @@ class SubscriptionCreate(TemplateResponseMixin, ModelFormMixin, ProcessFormView)
                    'subscriptions/subscription_email.txt',
                    {'subscription': self.object})
 
-        return HttpResponseRedirect(self.get_success_url())
+        return response
 
 
 new = SubscriptionCreate.as_view()
