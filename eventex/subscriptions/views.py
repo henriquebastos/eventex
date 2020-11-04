a8c2f0fb5ed13fa0253a374cb951b062917bd052
Giandoso <a15029@bcc.unifal-mg.edu.br>
2020-11-04 19:39:28 -0300

    Refactor install FormMixin

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -7,27 +7,27 @@ from django.template.loader import render_to_string
 from django.views import View
 from django.views.generic import DetailView
 from django.views.generic.base import TemplateResponseMixin
+from django.views.generic.edit import FormMixin
 
 from eventex.subscriptions.forms import SubscriptionForm
 from eventex.subscriptions.models import Subscription
 
 
-class SubscriptionCreate(TemplateResponseMixin, View):
+class SubscriptionCreate(TemplateResponseMixin, FormMixin, View):
     template_name = 'subscriptions/subscription_form.html'
     form_class = SubscriptionForm
 
     def get(self, *args, **kwargs):
+        self.object = None
         return self.render_to_response(self.get_context_data())
 
     def post(self, *args, **kwargs):
+        self.object = None
         form = self.get_form()
 
         if not form.is_valid():
             return self.form_invalid(form)
         return self.form_valid(form)
-
-    def form_invalid(self, form):
-        return self.render_to_response(self.get_context_data(form=form))
 
     def form_valid(self, form):
         self.object = form.save()
@@ -42,16 +42,6 @@ class SubscriptionCreate(TemplateResponseMixin, View):
 
     def get_success_url(self):
         return r('subscriptions:detail', self.object.pk)
-
-    def get_form(self):
-        if self.request.method == 'POST':
-            return self.form_class(self.request.POST)
-        return self.form_class()
-
-    def get_context_data(self, **kwargs):
-        context = dict(kwargs)
-        context.setdefault('form', self.get_form())
-        return context
 
 
 new = SubscriptionCreate.as_view()
