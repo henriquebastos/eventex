065923597dfd65ff23166099feeaba2815f32349
Giandoso <a15029@bcc.unifal-mg.edu.br>
2020-11-04 19:32:35 -0300

    SubscriptionModelTest.test_get_absolute_url

--- eventex/subscriptions/models.py
+++ eventex/subscriptions/models.py
@@ -1,19 +1,23 @@
 from django.db import models
 from eventex.subscriptions.validators import validate_cpf
+from django.shortcuts import resolve_url as r
 
 
 class Subscription(models.Model):
-	name = models.CharField('nome', max_length=100)
-	cpf = models.CharField('CPF', max_length=11, validators=[validate_cpf])
-	email = models.EmailField('e-mail', blank=True)
-	phone = models.CharField('telefone', max_length=20, blank=True)
-	created_at = models.DateTimeField('criado em', auto_now_add=True)
-	paid = models.BooleanField('pago', default=False)
+    name = models.CharField('nome', max_length=100)
+    cpf = models.CharField('CPF', max_length=11, validators=[validate_cpf])
+    email = models.EmailField('e-mail', blank=True)
+    phone = models.CharField('telefone', max_length=20, blank=True)
+    created_at = models.DateTimeField('criado em', auto_now_add=True)
+    paid = models.BooleanField('pago', default=False)
 
-	class Meta:
-		verbose_name_plural = 'inscrições'
-		verbose_name = 'inscrição'
-		ordering = ('-created_at',)
+    class Meta:
+        verbose_name_plural = 'inscrições'
+        verbose_name = 'inscrição'
+        ordering = ('-created_at',)
 
-	def __str__(self):
-		return self.name
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return r('subscriptions:detail', self.pk)
