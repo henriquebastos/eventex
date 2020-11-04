8a5e6a8fb982c66d780068e1b4b48a5693db5c77
Giandoso <a15029@bcc.unifal-mg.edu.br>
2020-11-04 18:42:34 -0300

    Refactor speaker_detail view

--- eventex/core/views.py
+++ eventex/core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.views.generic import DetailView
 from django.views.generic.list import ListView
 
 from eventex.core.models import Speaker, Talk
@@ -6,10 +6,6 @@ from eventex.core.models import Speaker, Talk
 home = ListView.as_view(template_name='index.html',
                         model=Speaker)
 
-
-def speaker_detail(request, slug):
-    speaker = get_object_or_404(Speaker, slug=slug)
-    return render(request, 'core/speaker_detail.html', {'speaker': speaker})
-
+speaker_detail = DetailView.as_view(model=Speaker)
 
 talk_list = ListView.as_view(model=Talk)
