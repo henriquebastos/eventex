a41573214269bda4ff8bb1281346945ea81373e4
Giandoso <a15029@bcc.unifal-mg.edu.br>
2020-11-05 09:42:26 -0300

    Refactor SubscriptionsCreate.send_mail

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -14,21 +14,20 @@ class SubscriptionCreate(CreateView):
 
     def form_valid(self, form):
         response = super().form_valid(form)
-
-        _send_mail('Confirmação de inscrição',
-                   settings.DEFAULT_FROM_EMAIL,
-                   self.object.email,
-                   'subscriptions/subscription_email.txt',
-                   {'subscription': self.object})
-
+        self.send_mail()
         return response
+
+    def send_mail(self):
+        subject = 'Confirmação de inscrição'
+        from_ = settings.DEFAULT_FROM_EMAIL
+        to = self.object.email
+        template_name = 'subscriptions/subscription_email.txt'
+        context = {'subscription': self.object}
+
+        body = render_to_string(template_name, context)
+        mail.send_mail(subject, body, from_, [from_, to])
 
 
 new = SubscriptionCreate.as_view()
 
 detail = DetailView.as_view(model=Subscription)
-
-
-def _send_mail(subject, from_, to, template_name, context):
-    body = render_to_string(template_name, context)
-    mail.send_mail(subject, body, from_, [from_, to])
